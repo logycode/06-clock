f5fbaa41d5811934199285b3bbc955e40cbba843
Tim Kranich <t.kranich@reply.de>
2023-01-22 10:59:33 +0100

    refactors time interval to use a setTime function

--- 06-clock.js
+++ 06-clock.js
@@ -6,32 +6,10 @@ clockInit();
 
 setInterval(() => {
   const date = new Date();
-  hours = "00" + date.getHours();
-  hours = hours.substring(hours.length - 2);
 
-  minutes = "00" + date.getMinutes();
-  minutes = minutes.substring(minutes.length - 2);
-
-  seconds = "00" + date.getSeconds();
-  seconds = seconds.substring(seconds.length - 2);
-
-  const hoursText = document.createTextNode(hours);
-  document.querySelector(".hours").textContent = hoursText.textContent;
-  document
-    .querySelector(".hand-hours")
-    .style.setProperty("--hours", angle(12, hours) + "deg");
-
-  const minutesText = document.createTextNode(minutes);
-  document.querySelector(".minutes").textContent = minutesText.textContent;
-  document
-    .querySelector(".hand-minutes")
-    .style.setProperty("--minutes", angle(60, minutes) + "deg");
-
-  const secondsText = document.createTextNode(seconds);
-  document.querySelector(".seconds").textContent = secondsText.textContent;
-  document
-    .querySelector(".hand-seconds")
-    .style.setProperty("--seconds", angle(60, seconds) + "deg");
+  setTime(date, "hours", hours);
+  setTime(date, "minutes", minutes);
+  setTime(date, "seconds", seconds);
 }, 1000);
 
 // blinking separators
@@ -40,10 +18,6 @@ setInterval(() => {
     el.classList.toggle("colon-off");
   });
 }, 1000);
-
-function angle(range, dimension) {
-  return (360 / range) * dimension;
-}
 
 function clockInit() {
   console.log("init");
@@ -54,4 +28,34 @@ function clockInit() {
     document.querySelector(".analog").appendChild(div);
     div.style.transform = "rotate(" + angle(12, i) + "deg) translateX(790%)";
   }
+}
+
+function angle(range, dimension) {
+  return (360 / range) * dimension;
+}
+
+function setTime(date, dimension, output) {
+  let range = 0;
+
+  if (dimension === "hours") {
+    output = date.getHours();
+    range = 12;
+  }
+  if (dimension === "minutes") {
+    output = date.getMinutes();
+    range = 60;
+  }
+  if (dimension === "seconds") {
+    output = date.getSeconds();
+    range = 60;
+  }
+
+  output = "00" + output;
+  output = output.substring(output.length - 2);
+
+  const text = document.createTextNode(output);
+  document.querySelector("." + dimension).textContent = text.textContent;
+  document
+    .querySelector(".hand-" + dimension)
+    .style.setProperty("--" + dimension, angle(range, output) + "deg");
 }
